0aac872d169f34729f3d46f355a16d52e7cdb5c8
Vitor <vitor251093@gmail.com>
2018-11-10 14:14:56 -0200

    Started to add conditionals for initial requests

--- darkspore_server/main.py
+++ darkspore_server/main.py
@@ -2,6 +2,7 @@ import sys
 import getopt
 import warnings
 import timeit
+import json
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -28,25 +29,38 @@ def favicon():
 @app.route("/api", methods=['GET','POST'])
 def api():
     print request.args
-    version     = request.args.get('version',  default='1')
+    #version     = request.args.get('version',  default='1')
     callback    = request.args.get('callback', default='')
     method      = request.args.get('method',   default='')
-    format_type = request.args.get('format',   default='json')
+    #format_type = request.args.get('format',   default='json')
     
-    # server.run()
+    # Method that checks the status of the server; it's called each 10 or 20 seconds by the launcher
+    if method == 'api.status.getStatus':
+        if callback == 'updateServerStatus(data)':
+            data = {}
+            #data['response'] = {}
+            #data['response']['state'] = 'kSessionStateConnected'
+            json_data = json.dumps(data)
+            return json_data
     
     return "{}"
 
 @app.route("/bootstrap/api", methods=['GET','POST'])
 def bootstrapApi():
     print request.args
-    version = request.args.get('version', default='1')
+    #version = request.args.get('version', default='1')
     method  = request.args.get('method',  default='')
     build   = request.args.get('build',   default='')
-    include_patches  = (request.args.get('include_patches',  default='true') == 'true')
-    include_settings = (request.args.get('include_settings', default='true') == 'true')
+    #include_patches  = (request.args.get('include_patches',  default='true') == 'true')
+    #include_settings = (request.args.get('include_settings', default='true') == 'true')
 
-    # server.run()
+    # First method called by the game on startup
+    if method == 'api.config.getConfigs':
+        data = {}
+        #data['to_version'] = build
+        #data['from_version'] = build
+        json_data = json.dumps(data)
+        return json_data
     
     return "{}"
 
@@ -58,7 +72,8 @@ def bootstrapLauncherNotes():
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def otherRequests(path):
     print request.args
-    return 'You want path: %s' % path
+    print 'You want path: %s' % path
+    return ""
 
 if __name__ == "__main__":
     # Needed for Flask is the Python file is called directly

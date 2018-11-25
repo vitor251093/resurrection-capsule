43b07c37ff688e6b3cd9418f2344295b6aed626b
Vitor <vitor251093@gmail.com>
2018-11-25 18:39:20 -0200

    Improvements in the code and fix of the favicon bug

--- darkspore_server/main.py
+++ darkspore_server/main.py
@@ -1,3 +1,5 @@
+from models.account import *
+import os
 import sys
 import getopt
 import warnings
@@ -10,33 +12,68 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import Response
+from flask import send_from_directory
 import logging
+
+class DarkSporeServerConfig(object):
+    @staticmethod
+    def shouldSkipLauncher():
+        return True
 
 class DarkSporeServer(object):
     def __init__(self):
-        print "Starting..."
+        self.accounts = {}
+        self.accountsSequenceNext = 0
 
     def getAccount(self, id):
+        return self.accounts[str(id)]
+
+    def createAccount(self, email, name):
+        id = self.accountsSequenceNext
+        if self.accounts[str(id)] != None:
+            return -1
+
+        self.accounts[str(id)] = Account(id, email, name)
+        self.accountsSequenceNext += 1
+        return id
+
+class DarkSporeServerApi(object):
+    @staticmethod
+    def getStatus_javascript(callback):
+        javascript = ("var data = {status: {blaze: {health: 1}, gms: {health: 1}, nucleus: {health: 1}, game: {health: 1}}}; " +
+                      "setPlayButtonActive(); " +
+                      "setTimeout(function(){" +
+                          "updateBottomleftProgressComment('Local server enabled');" +
+                          "updateProgressBar(1);" +
+                      "},100); " +
+                      callback + ";")
+        if DarkSporeServerConfig.shouldSkipLauncher():
+            javascript = "clickPlayButton();"
+        return javascript
+
+    @staticmethod
+    def getAccount_object(id):
         print "Retrieving user info..."
+        account = server.getAccount(id)
         return {
             "avatar_id": '1', # TODO
             "avatar_updated": '0', # Not sure of what is that
-            "blaze_id": '1', # Not sure of what is that
-            "chain_progression": '0', # Not sure of what is that
+            "blaze_id": str(account.id),
+            "chain_progression": str(account.chainProgression()), 
             "creature_rewards": '0', # Not sure of what is that
             "current_game_id": '1', # Not sure of what is that
             "current_playgroup_id": '0', # Not sure of what is that
             "default_deck_pve_id": '1', # Not sure of what is that
             "default_deck_pvp_id": None, # Not sure of what is that
             "dna": '0', # TODO
-            "email": 'tom@mywebsite.com', # TODO
-            "id": str(id),
-            "level": '3', # TODO
-            "name": 'somegamerdood', # TODO
+            "email": account.email,
+            "id": str(account.id),
+            "level": str(account.level),
+            "name": account.name,
             "new_player_inventory": '1', # Not sure of what is that
             "new_player_progress": '7000', # Not sure of what is that
             "nucleus_id": '1', # Not sure of what is that
-            "tutorial_completed": 'Y', # TODO
+            "tutorial_completed": ('Y' if account.tutorialCompleted else 'N'),
             "unlock_catalysts": '0', # Not sure of what is that
             "unlock_diagonal_catalysts": '0', # Not sure of what is that
             "unlock_editor_flair_slots": '3', # Not sure of what is that
@@ -60,6 +97,10 @@ def jsonResponseWithObject(obj):
     json_data = json.dumps(obj)
     return Response(json_data, mimetype='application/json')
 
+def xmlResponseWithXmlElement(xmlElement):
+    tree_str = ElementTree.tostring(xmlElement, encoding='utf8', method='xml')
+    return Response(tree_str, mimetype='text/xml')
+
 @app.route("/api", methods=['GET','POST'])
 def api():
     method      = request.args.get('method',   default='')
@@ -69,10 +110,7 @@ def api():
 
     if method == 'api.status.getStatus':
         if callback == 'updateServerStatus(data)' and version == '1' and format_type == 'json':
-            javascript = ("var data = {status: {blaze: {health: 1}, gms: {health: 1}, nucleus: {health: 1}, game: {health: 1}}}; " +
-                          "setPlayButtonActive(); " +
-                          "setTimeout(function(){updateBottomleftProgressComment('Local server enabled');updateProgressBar(1);},100); " +
-                          callback + ";")
+            javascript = DarkSporeServerApi.getStatus_javascript(callback)
             return Response(javascript, mimetype='application/javascript')
 
     print " "
@@ -93,7 +131,7 @@ def bootstrapApi():
     version = request.args.get('version', default='')
     build   = request.args.get('build',   default='')
 
-    # api.account.getAccount progress: 0%
+    # api.account.getAccount progress: 3.5%
     if method == 'api.account.getAccount':
         include_feed  = (request.args.get('include_feed',  default='') == 'true')
         include_decks = (request.args.get('include_decks', default='') == 'true')
@@ -107,7 +145,7 @@ def bootstrapApi():
         xml_tree.SubElement(root, "exectime").text = '1' # Not sure of what is that
 
         account = xml_tree.SubElement(root, "account")
-        accountInfo = server.getAccount(player_id)
+        accountInfo = DarkSporeServerApi.getAccount_object(player_id)
         for key,val in accountInfo.items():
             if val is None:
                 xml_tree.SubElement(account, key)
@@ -115,9 +153,15 @@ def bootstrapApi():
                 xml_tree.SubElement(account, key).text = val
 
         # Still missing creatures, decks and feed
+        if include_feed:
+            print "include feed"
+        if include_decks:
+            print "include decks"
+        if include_creatures:
+            print "include creatures"
 
-        tree_str = ElementTree.tostring(root, encoding='utf8', method='xml')
-        return Response(tree_str, mimetype='text/xml')
+        return xmlResponseWithXmlElement(root)
+
 
     # api.config.getConfigs progress: 0%
     if method == 'api.config.getConfigs':
@@ -125,39 +169,92 @@ def bootstrapApi():
         include_settings = (request.args.get('include_settings', default='') == 'true')
 
         root = xml_tree.Element("response")
+        xml_tree.SubElement(root, "stat").text = 'ok'
+        xml_tree.SubElement(root, "version").text = version
+        xml_tree.SubElement(root, "timestamp").text = str(long(time.time()))
+        xml_tree.SubElement(root, "exectime").text = '1' # Not sure of what is that
 
-        xml_tree.SubElement(root, "to_version").text = build
-        xml_tree.SubElement(root, "from_version").text = build
-        #xml_tree.SubElement(root, "date").text = ''
-        #xml_tree.SubElement(root, "target").text = ''
+        configs = xml_tree.SubElement(root, "configs")
+        config = xml_tree.SubElement(configs, "config")
+        xml_tree.SubElement(config, "open").text = 'true'
+        xml_tree.SubElement(config, "launcher_url").text = 'http://127.0.0.1/'
+        xml_tree.SubElement(config, "launcher_action").text = 'x'
+        xml_tree.SubElement(config, "liferay_host").text = '127.0.0.1'
+        xml_tree.SubElement(config, "liferay_port").text = '57371' #LSDS1 / Local Server Darkspore 1
+        xml_tree.SubElement(config, "sporenet_host").text = '127.0.0.1'
+        xml_tree.SubElement(config, "sporenet_port").text = '57372' #LSDS2
+        xml_tree.SubElement(config, "sporenet_db_name").text = 'darkspore'
+        xml_tree.SubElement(config, "sporenet_db_port").text = '57373' #LSDS3
+        xml_tree.SubElement(config, "sporenet_db_host").text = '127.0.0.1'
+        xml_tree.SubElement(config, "blaze_env").text = 'production' # Reblaze to protect the servers?
+        xml_tree.SubElement(config, "blaze_secure").text = 'low'
+        xml_tree.SubElement(config, "blaze_service_name").text = 'darkspore'
+
         if include_patches:
             xml_tree.SubElement(root, "patches")
-        #xml_tree.SubElement(root, "launcher_url").text = ''
-        #xml_tree.SubElement(root, "launcher_action").text = ''
-        settings = xml_tree.SubElement(root, "settings")
-        xml_tree.SubElement(settings, "liferay_host").text = '127.0.0.1'
-        xml_tree.SubElement(settings, "liferay_port").text = '80'
-        xml_tree.SubElement(settings, "sporenet_host").text = '127.0.0.1'
-        xml_tree.SubElement(settings, "sporenet_port").text = '80'
-        xml_tree.SubElement(settings, "sporenet_db_name").text = 'darkspore'
-        xml_tree.SubElement(settings, "sporenet_db_port").text = '80'
-        xml_tree.SubElement(settings, "sporenet_db_host").text = '127.0.0.1'
-        xml_tree.SubElement(settings, "blaze_env").text = ''
-        xml_tree.SubElement(settings, "blaze_secure").text = ''
-        xml_tree.SubElement(settings, "blaze_service_name").text = ''
 
-        tree_str = ElementTree.tostring(root, encoding='utf8', method='xml')
-        return Response(tree_str, mimetype='text/xml')
+        if include_settings:
+            settings = xml_tree.SubElement(root, "settings")
+            xml_tree.SubElement(settings, "active").text = 'true'
+            xml_tree.SubElement(settings, "locale").text = 'en-US'
+            xml_tree.SubElement(settings, "sessionId").text = '1'
+            xml_tree.SubElement(settings, "token").text = 'abcdef0123456789'
+            xml_tree.SubElement(settings, "count").text = '1'
+            xml_tree.SubElement(settings, "round_id").text = '1'
+            xml_tree.SubElement(settings, "reference_id").text = '1'
+            xml_tree.SubElement(settings, "new_player_inventory").text = '1'
+            xml_tree.SubElement(settings, "new_player_progress").text = '0'
+
+        return xmlResponseWithXmlElement(root)
 
     return jsonResponseWithObject({})
+
+@app.route("/game/service/png", methods=['GET','POST'])
+def gameServicePng():
+    account_id = request.args.get('account_id')
+    if account_id != None:
+        print("Should return user account PNG")
+        return ""
+
+    creature_id = request.args.get('creature_id')
+    if creature_id != None:
+        print("Should return creature PNG")
+        return ""
+    
+    template_id = request.args.get('template_id')
+    if template_id != None:
+        size = request.args.get('size')
+        print("Should return creature template PNG")
+        return ""
+
+    ability_id = request.args.get('ability_id')
+    if ability_id != None:
+        print("Should return ability PNG")
+        return ""
+
+    rigblock_id = request.args.get('rigblock_id')
+    if rigblock_id != None:
+        # Rig blocks are:
+        # Eyes, noses, mouths, hands, feet, tails, wings, armor, 
+        # weapons, jewelry and all types of assets used in character creation.
+        print("Should return creature part PNG")
+        return ""
+
+    print " "
+    print "http://" + request.host + "/game/service/png"
+    print request.args
+    print " "
+
+    return ""
 
 @app.route("/bootstrap/launcher/notes")
 def bootstrapLauncherNotes():
     return render_template('launcher_notes.html')
 
-@app.route("/favicon.ico")
+@app.route('/favicon.ico')
 def favicon():
-    return render_template('favicon.ico')
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/")
 def index():

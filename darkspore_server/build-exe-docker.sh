a9e1622d7b679b2c2404623d96b7f7880ef09bb4
Vitor <vitor251093@gmail.com>
2019-03-29 14:20:29 -0300

    Added precaution if dist folder does not exist

--- darkspore_server/build-exe-docker.sh
+++ darkspore_server/build-exe-docker.sh
@@ -1,4 +1,5 @@
 #cxfreeze /darkspore_server/hello.py --target-dir /darkspore_server_storage/dist --target-name=DarksporeServer.exe
 cd /darkspore_server
-rm -r /darkspore_server_storage/dist/*
+if [ -d "/darkspore_server_storage/dist" ]; then rm -Rf /darkspore_server_storage/dist; fi
+mkdir /darkspore_server_storage/dist
 python buildexe.py build_exe -b /darkspore_server_storage/dist
